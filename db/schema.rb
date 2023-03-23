87a189d31afaf1b558bd21f02d67da53326270b2
Lionel Diot <diot.lionel2@gmail.com>
2023-03-23 05:17:24 +0100

    implementation code club modification du devise controller pour ajouter les parametres indispensables à l'inscription et eviter les conflits de nil sur des valeurs sensé être toujours presente (sexe date age etc...)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_002329) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_035241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_22_002329) do
     t.boolean "week_night", default: false
     t.boolean "weekend_day", default: false
     t.boolean "weekend_night", default: false
+    t.string "code_club"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
