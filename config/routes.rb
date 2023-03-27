27e2dcdf0b28eabbcf03e3f78d1f88bbb2e8e467
Sonia <soniapavageau@gmail.com>
2023-03-27 12:49:42 +0200

    suppression routes prototypes

--- config/routes.rb
+++ config/routes.rb
@@ -17,18 +17,4 @@ Rails.application.routes.draw do
 
   get 'users_search', to: 'users#search', as: 'users_search'
   get 'users_selection', to:'users#selection', as:'users_selection'
-
-  # UI prototypes / to be deleted once applied in views
-  # get 'prototypes/signin', to: 'prototypes#signin'
-  # get 'prototypes/signup', to: 'prototypes#signup'
-  # get 'prototypes/settings', to: 'prototypes#settings_index'
-  # get 'prototypes/settings/photo', to: 'prototypes#settings_photo'
-  # get 'prototypes/settings/informations', to: 'prototypes#settings_informations'
-  # get 'prototypes/settings/player_preferences', to: 'prototypes#settings_player_preferences'
-  # get 'prototypes/settings/account', to: 'prototypes#settings_account'
-  # get 'prototypes/search', to: 'prototypes#search'
-  # get 'prototypes/results', to: 'prototypes#results'
-  # get 'prototypes/player', to: 'prototypes#player'
-  # get 'prototypes/chatroom', to: 'prototypes#chatroom'
-  # get 'prototypes/invitations', to: 'prototypes#invitations'
 end
