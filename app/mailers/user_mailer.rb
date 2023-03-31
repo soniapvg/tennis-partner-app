5af0826226796e4f46ba57b672b1da75eaff502f
ghislain <genet.ghislain@gmail.com>
2023-03-31 10:42:44 +0200

    mailer fonctionnel

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,13 @@ class UserMailer < ApplicationMailer
         @user = user 
     
         #on définit une variable @url qu'on utilisera dans la view d’e-mail
-        @url  = 'tennis-club-partner.herokuapp.com/' 
+        if Rails.env.production?
+          @url = 'https://tennis-club-partner.herokuapp.com/'
+        elsif Rails.env.development?
+          @url = 'http://localhost:3000'
+        else
+          @url = 'https://tennis-club-partner-staging.herokuapp.com/'
+        end
     
         # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
         mail(to: @user.email, subject: 'Bienvenue chez nous !') 
